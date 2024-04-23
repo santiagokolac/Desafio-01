3ed8ed3cd92a2e9cdea8509941b07691e69b9aad
Santiago Kolac <kolac.santiago1@gmail.com>
2024-04-23 14:59:48 -0300

    Desafio 2

---
+++ ProductManager.js
@@ -0,0 +1,102 @@
+const fs = require("fs");
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async addProduct(product) {
+    try {
+      const products = await this.getProductsFromFile();
+      product.id = this.generateUniqueId(products);
+      products.push(product);
+      await this.saveProductsToFile(products);
+    } catch (error) {
+      console.error("Error al agregar producto:", error);
+    }
+  }
+
+  async getProducts() {
+    try {
+      const products = await this.getProductsFromFile();
+      return products;
+    } catch (error) {
+      console.error("Error al obtener producto:", error);
+      return [];
+    }
+  }
+
+  async getProductById(productId) {
+    try {
+      const products = await this.getProductsFromFile();
+      const product = products.find((p) => p.id === productId);
+      if (product) {
+        return product;
+      } else {
+        console.log("Producto no encontrado");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error al obtener producto por id", error);
+      return null;
+    }
+  }
+
+  async updateProduct(productId, updatedFields) {
+    try {
+      let products = await this.getProductsFromFile();
+      const index = products.findIndex((p) => p.id === productId);
+      if (index !== -1) {
+        products[index] = { ...products[index], ...updatedFields };
+        await this.saveProductsToFile(products);
+        console.log("Producto actualizado exitosamente");
+      } else {
+        console.log("Producto no encontrado");
+      }
+    } catch (error) {
+      console.error("Error al actualizar producto:", error);
+    }
+  }
+
+  async deleteProduct(productId) {
+    try {
+      let products = await this.getProductsFromFile();
+      products = products.filter((p) => p.id !== productId);
+      await this.saveProductsToFile(products);
+      console.log("Producto eliminado exitosamente");
+    } catch (error) {
+      console.error("Error al borrar producto", error);
+    }
+  }
+
+  async getProductsFromFile() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const productsData = await fs.promises.readFile(this.path, "utf8");
+        return JSON.parse(productsData);
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error("Error al leer el archivo de productos", error);
+      return [];
+    }
+  }
+
+  async saveProductsToFile(products) {
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+      console.log("Productos guardados en el archivo exitosamente");
+    } catch (error) {
+      console.error("Error al guardar productos en el archivo:", error);
+    }
+  }
+
+  generateUniqueId(products) {
+    if (products.length === 0) return 1;
+    const maxId = Math.max(...products.map((p) => p.id));
+    return maxId + 1;
+  }
+}
+
+module.exports = ProductManager;
--- desafio-01.js
+++ desafio-01.js
@@ -1,81 +1,37 @@
-class ProductManager {
-  constructor() {
-    this.products = [];
-  }
+const ProductManager = require("./ProductManager");
 
-  addProduct(title, description, price, thumbnail, code, stock) {
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-      console.log("Por favor rellene todos los campos");
-      return;
-    }
+const productManager = new ProductManager("./products.json");
 
-    if (this.products.some((product) => product.code === code)) {
-      console.log("Ya existe un producto con ese código");
-      return;
-    }
+const product = {
+  title: "Celular",
+  description: "Celular de alta gama",
+  price: 400000,
+  thumbnail: "/images/celular.png",
+  code: "celular-01",
+  stock: 50,
+};
 
-    const product = {
-      id: this.getMaxId() + 1,
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock,
-    };
-
-    this.products.push(product);
-  }
-
-  getMaxId() {
-    let maxId = 0;
-    this.products.forEach((product) => {
-      if (product.id > maxId) maxId = product.id;
-    });
-    return maxId;
-  }
-
-  getProducts() {
-    return this.products;
-  }
-
-  getProductById(productId) {
-    const product = this.products.find((product) => product.id === productId);
-    if (product) {
-      return product;
-    } else {
-      console.log("Producto no encontrado");
-    }
-  }
-}
-
-const productManager = new ProductManager();
-
-productManager.addProduct(
-  "Celular",
-  "Celular de alta gama",
-  350000,
-  "/images/phone01.png",
-  "phone01",
-  100
-);
-productManager.addProduct(
-  "Notebook",
-  "Notebook gamer",
-  2000000,
-  "/images/notebook01.png",
-  "notebook01",
-  50
-);
-productManager.addProduct(
-  "Tablet",
-  "Tablet de gama media",
-  200000,
-  "/images/tablet01.png",
-  "tablet01",
-  75
-);
-
-console.log(productManager.getProducts());
-console.log(productManager.getProductById(2));
-console.log(productManager.getProductById(4));
+productManager
+  .addProduct(product)
+  .then(() => {
+    console.log("Producto agregado exitosamente");
+    return productManager.getProducts();
+  })
+  .then((products) => {
+    console.log("Todos los productos:", products);
+    return productManager.getProductById(1);
+  })
+  .then((product) => {
+    console.log("Producto con id 1:", product);
+    return productManager.updateProduct(1, { price: 900 });
+  })
+  .then(() => {
+    console.log("Producto actualizado exitosamente");
+    return productManager.deleteProduct(1);
+  })
+  .then(() => {
+    console.log("Producto eliminado exitosamente");
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
